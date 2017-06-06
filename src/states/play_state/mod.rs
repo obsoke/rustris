24e02f7089dc2f7aaa6728f73322b493ae1462de
Dale Karp <dale@dale.io>
2017-06-06 16:53:14 -0400

    attempting to fix input

--- src/states/play_state/mod.rs
+++ src/states/play_state/mod.rs
@@ -89,7 +89,7 @@ pub struct PlayState {
 
 
 impl PlayState {
-    pub fn new (ctx: &mut Context) -> GameResult<PlayState> {
+    pub fn new(ctx: &mut Context) -> GameResult<PlayState> {
         let image = graphics::Image::new(ctx, "/block.png")?;
         let mut bag = PieceBag::new();
         let first_piece = bag.take_piece();
@@ -120,17 +120,13 @@ impl PlayState {
                 self.current_piece.potential_top_left.x -= 1;
                 self.input.left.delay_timer = 0.0;
             }
-        }
-        else if self.input.right.is_active {
+        } else if self.input.right.is_active {
             self.input.right.delay_timer += dt.as_subsec_millis();
             if self.input.right.delay_timer >= INPUT_DELAY_TIME {
-                println!("moved right");
                 self.current_piece.potential_top_left.x += 1;
                 self.input.right.delay_timer = 0.0;
             }
-        }
-        else if self.input.soft_drop.is_active {
-            println!("soft_drop");
+        } else if self.input.soft_drop.is_active {
             self.input.soft_drop.delay_timer += dt.as_subsec_millis();
             if self.input.soft_drop.delay_timer >= INPUT_DELAY_TIME {
                 self.current_piece.potential_top_left.y += 1;
@@ -153,11 +149,8 @@ impl PlayState {
             self.fall_timer = 0.0;
             self.current_piece.potential_top_left.y += 1;
 
-            println!("normal");
-            let did_land = self.well.check_for_landing(
-                &current_shape,
-                &self.current_piece.potential_top_left
-            );
+            let did_land = self.well
+                .check_for_landing(&current_shape, &self.current_piece.potential_top_left);
 
             if did_land {
                 if self.current_piece.top_left.y < 2 {
@@ -167,7 +160,6 @@ impl PlayState {
                 }
 
                 // game isn't over - take another piece and move to next frame
-                println!("piece landing");
                 self.well.land(&self.current_piece);
                 self.current_piece = self.bag.take_piece();
                 return Ok(false);
@@ -185,14 +177,11 @@ impl PlayState {
         let mut potential_shadow_position = shadow_position;
         loop {
             potential_shadow_position.y += 1;
-            println!("shadow");
-            let collision_found = self.well.check_for_landing(
-                &self.current_piece.get_shape(),
-                &potential_shadow_position
-            );
+            let collision_found = self.well
+                .check_for_landing(&self.current_piece.get_shape(), &potential_shadow_position);
 
             if collision_found {
-                break
+                break;
             }
 
             shadow_position = potential_shadow_position;
@@ -204,7 +193,7 @@ impl PlayState {
 
     fn handle_lines_clears(&mut self) -> GameResult<()> {
         // check for line clears
-        for r in (0 .. self.well.data.len()).rev() {
+        for r in (0..self.well.data.len()).rev() {
             let mut is_row_filled = true;
             for (c, _) in self.well.data[r].iter().enumerate() {
                 if self.well.data[r][c] == 0 {
@@ -225,9 +214,11 @@ impl PlayState {
     }
 
     fn handle_collisions(&mut self) -> GameResult<()> {
-        if self.input.left.is_active || self.input.right.is_active || self.input.soft_drop.is_active {
+        if self.input.left.is_active || self.input.right.is_active ||
+           self.input.soft_drop.is_active {
             let current_shape = self.current_piece.get_shape();
-            let collision_found = self.well.check_for_collisions(&current_shape, &self.current_piece.potential_top_left);
+            let collision_found = self.well
+                .check_for_collisions(&current_shape, &self.current_piece.potential_top_left);
 
             if collision_found {
                 self.current_piece.potential_top_left = self.current_piece.top_left;
@@ -235,31 +226,32 @@ impl PlayState {
 
             self.current_piece.top_left = self.current_piece.potential_top_left; // advance tetromino
             Ok(())
-        }
-        else if self.input.rotate_clockwise.is_active || self.input.rotate_counterclockwise.is_active {
+        } else if self.input.rotate_clockwise.is_active ||
+                  self.input.rotate_counterclockwise.is_active {
             let next_shape = self.current_piece.get_next_shape();
-            let collision_found = self.well.check_for_collisions(&next_shape, &self.current_piece.top_left);
+            let collision_found = self.well
+                .check_for_collisions(&next_shape, &self.current_piece.top_left);
 
             if !collision_found {
                 self.current_piece.change_shape();
-            }
-            else {
+            } else {
                 // wall kick attempt!
                 // need to do 2 checks:
                 // move one piece to the right & perform all above checks
                 let mut potential_position = self.current_piece.top_left; // creates a copy of 'Position' struct
                 potential_position.x += 1;
-                let collision_found = self.well.check_for_collisions(&next_shape, &potential_position);
+                let collision_found = self.well
+                    .check_for_collisions(&next_shape, &potential_position);
 
                 if !collision_found {
                     self.current_piece.top_left = potential_position;
                     self.current_piece.potential_top_left = potential_position;
                     self.current_piece.change_shape();
-                }
-                else {
+                } else {
                     let mut potential_position = self.current_piece.top_left;
                     potential_position.x -= 1;
-                    let collision_found = self.well.check_for_collisions(&next_shape, &potential_position);
+                    let collision_found = self.well
+                        .check_for_collisions(&next_shape, &potential_position);
 
                     if !collision_found {
                         self.current_piece.top_left = potential_position;
@@ -270,16 +262,13 @@ impl PlayState {
             }
 
             Ok(())
-        }
-        else if self.input.hard_drop.is_active {
+        } else if self.input.hard_drop.is_active {
             self.current_piece.top_left = self.current_piece.get_shadow_position();
-            println!("hard drop");
             self.well.land(&self.current_piece);
             self.current_piece = self.bag.take_piece();
 
             Ok(())
-        }
-        else {
+        } else {
             Ok(())
         }
     }
@@ -295,8 +284,6 @@ impl event::EventHandler for PlayState {
             return Ok(());
         }
 
-        println!("Value of current command: {:?}", self.current_command);
-        println!("Value of Left: {:?}", self.input.left);
         // TODO: handle/respond user input
         self.handle_user_input(dt)?;
 
@@ -321,7 +308,8 @@ impl event::EventHandler for PlayState {
         graphics::clear(ctx);
 
         self.well.draw(ctx, &self.image)?;
-        self.current_piece.draw_shadow(ctx, &self.image, &self.current_piece.get_shadow_position())?;
+        self.current_piece
+            .draw_shadow(ctx, &self.image, &self.current_piece.get_shadow_position())?;
         self.current_piece.draw(ctx, &self.image)?;
 
         graphics::present(ctx);
@@ -329,13 +317,20 @@ impl event::EventHandler for PlayState {
         Ok(())
     }
 
-    fn key_down_event(&mut self, keycode: Keycode, _keymod: Mod, _repeat: bool) {
-        //println!("key hit: {:?}", keycode);
+    fn key_down_event(&mut self, keycode: Keycode, _keymod: Mod, repeat: bool) {
+        println!("key down event occured:");
         match keycode {
             Keycode::Left => {
-                self.input.left.is_active = true;
-                self.current_command = Command::Left;
-            },
+                println!("repeat of Left: {}", repeat);
+                if repeat {
+                    self.input.left.is_active = false;
+                    self.input.left.delay_timer = INPUT_DELAY_TIME;
+
+                } else {
+                    self.input.left.is_active = true;
+                    self.current_command = Command::Left;
+                }
+            }
             Keycode::Right => self.input.right.is_active = true,
             Keycode::Up => self.input.hard_drop.is_active = true,
             Keycode::Down => self.input.soft_drop.is_active = true,
@@ -351,7 +346,7 @@ impl event::EventHandler for PlayState {
             Keycode::Left => {
                 self.input.left.is_active = false;
                 self.input.left.delay_timer = INPUT_DELAY_TIME;
-            },
+            }
             Keycode::Right => self.input.right.is_active = false,
             Keycode::Up => self.input.hard_drop.is_active = false,
             Keycode::Down => self.input.soft_drop.is_active = false,
