ac0a423a3a14829c646e88fc5606f809667d74d7
Dale Karp <dale@dale.io>
2017-06-15 00:24:43 -0400

    basic state manager with inspiration from ggez_goodies & amethyst

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,15 @@
 extern crate ggez;
+extern crate sdl2;
 
 mod states;
 
-use ggez::{Context, event, conf};
+use ggez::{Context, GameResult, conf, timer};
+use ggez::event::EventHandler;
+use sdl2::{keyboard, event as SdlEvent};
+use sdl2::event::Event::*;
+
 use states::play_state::PlayState;
+use states::StateManager;
 
 fn main() {
     let mut conf = conf::Conf::new();
@@ -14,9 +20,98 @@ fn main() {
 
     let ctx = &mut Context::load_from_conf("rustris", "obsoke", conf).expect("Could not load configuartion");
 
-    let state = &mut PlayState::new(ctx).expect("Could not initialize state");
+    let state = PlayState::new(ctx).expect("Could not initialize state");
 
-    if let Err(e) = event::run(ctx, state) {
+    if let Err(e) = run(ctx, state) {
         println!("Error encountered: {}", e);
     }
+}
+
+
+/// Runs the game's main loop, calling event callbacks on the given state
+/// object as events occur.
+///
+/// It does not try to do any type of framerate limiting.  See the
+/// documentation for the `timer` module for more info.
+pub fn run<S>(ctx: &mut Context, state: S) -> GameResult<()>
+    where S: EventHandler + 'static
+{
+    {
+        let mut state_manager = StateManager::new(state);
+        let mut event_pump = ctx.sdl_context.event_pump()?;
+
+        let mut continuing = true;
+        while continuing {
+            ctx.timer_context.tick();
+
+            for event in event_pump.poll_iter() {
+                match event {
+                    Quit { .. } => {
+                        continuing = state_manager.quit_event();
+                        // println!("Quit event: {:?}", t);
+                    }
+                    KeyDown {
+                        keycode,
+                        keymod,
+                        repeat,
+                        ..
+                    } => {
+                        if let Some(key) = keycode {
+                            if key == keyboard::Keycode::Escape {
+                                ctx.quit()?;
+                            } else {
+                                state_manager.key_down_event(key, keymod, repeat)
+                            }
+                        }
+                    }
+                    KeyUp {
+                        keycode,
+                        keymod,
+                        repeat,
+                        ..
+                    } => {
+                        if let Some(key) = keycode {
+                            state_manager.key_up_event(key, keymod, repeat)
+                        }
+                    }
+                    MouseButtonDown { mouse_btn, x, y, .. } => {
+                        state_manager.mouse_button_down_event(mouse_btn, x, y)
+                    }
+                    MouseButtonUp { mouse_btn, x, y, .. } => {
+                        state_manager.mouse_button_up_event(mouse_btn, x, y)
+                    }
+                    MouseMotion {
+                        mousestate,
+                        x,
+                        y,
+                        xrel,
+                        yrel,
+                        ..
+                    } => state_manager.mouse_motion_event(mousestate, x, y, xrel, yrel),
+                    MouseWheel { x, y, .. } => state_manager.mouse_wheel_event(x, y),
+                    ControllerButtonDown { button, which, .. } => {
+                        state_manager.controller_button_down_event(button, which)
+                    }
+                    ControllerButtonUp { button, which, .. } =>
+                        state_manager.controller_button_up_event(button, which),
+                    ControllerAxisMotion { axis, value, which, .. } => {
+                        state_manager.controller_axis_event(axis, value, which)
+                    }
+                    Window { win_event: SdlEvent::WindowEvent::FocusGained, .. } => {
+                        state_manager.focus_event(true)
+                    }
+                    Window { win_event: SdlEvent::WindowEvent::FocusLost, .. } => {
+                        state_manager.focus_event(false)
+                    }
+                    _ => {}
+                }
+            }
+
+            let dt = timer::get_delta(ctx);
+            state_manager.update(ctx, dt)?;
+            state_manager.draw(ctx)?;
+        }
+    }
+
+    Ok(())
 }
--- src/states/mod.rs
+++ src/states/mod.rs
@@ -1,1 +1,124 @@
 pub mod play_state;
+
+use std::time::Duration;
+
+use sdl2::mouse;
+
+use ggez::{Context, GameResult};
+use ggez::event::{EventHandler, Keycode, Mod, Button, Axis};
+
+/// A state manager
+pub struct StateManager {
+    running: bool,
+    states: Vec<Box<EventHandler>>
+}
+
+impl StateManager {
+    pub fn new<S>(state: S) -> Self where S: EventHandler + 'static {
+        Self {
+            running: true,
+            states: vec![Box::new(state)], // create empty state stack
+        }
+    }
+
+    pub fn is_running(&self) -> bool {
+        self.running
+    }
+}
+
+impl EventHandler for StateManager {
+    fn update(&mut self, ctx: &mut Context, dt: Duration) -> GameResult<()> {
+        match self.states.last_mut() {
+            Some(state) => state.update(ctx, dt),
+            None => Ok(()),
+        }
+    }
+    fn draw(&mut self, ctx: &mut Context) -> GameResult<()> {
+        match self.states.last_mut() {
+            Some(state) => state.draw(ctx),
+            None => Ok(()),
+        }
+    }
+    fn mouse_button_down_event(&mut self, _button: mouse::MouseButton, _x: i32, _y: i32) {
+        match self.states.last_mut() {
+            Some(state) => state.mouse_button_down_event(_button, _x, _y),
+            None => (),
+        }
+    }
+
+    fn mouse_button_up_event(&mut self, _button: mouse::MouseButton, _x: i32, _y: i32) {
+        match self.states.last_mut() {
+            Some(state) => state.mouse_button_up_event(_button, _x, _y),
+            None => (),
+        }
+    }
+
+    fn mouse_motion_event(&mut self,
+                          _state: mouse::MouseState,
+                          _x: i32,
+                          _y: i32,
+                          _xrel: i32,
+                          _yrel: i32) {
+        match self.states.last_mut() {
+            Some(state) => state.mouse_motion_event(_state, _x, _y, _xrel, _yrel),
+            None => (),
+        }
+
+    }
+
+    fn mouse_wheel_event(&mut self, _x: i32, _y: i32) {
+        match self.states.last_mut() {
+            Some(state) => state.mouse_wheel_event(_x, _y),
+            None => (),
+        }
+    }
+
+    fn key_down_event(&mut self, _keycode: Keycode, _keymod: Mod, _repeat: bool) {
+        match self.states.last_mut() {
+            Some(state) => state.key_down_event(_keycode, _keymod, _repeat),
+            None => (),
+        }
+    }
+
+    fn key_up_event(&mut self, _keycode: Keycode, _keymod: Mod, _repeat: bool) {
+        match self.states.last_mut() {
+            Some(state) => state.key_up_event(_keycode, _keymod, _repeat),
+            None => (),
+        }
+    }
+
+    fn controller_button_down_event(&mut self, _btn: Button, _instance_id: i32) {
+        match self.states.last_mut() {
+            Some(state) => state.controller_button_down_event(_btn, _instance_id),
+            None => (),
+        }
+    }
+    fn controller_button_up_event(&mut self, _btn: Button, _instance_id: i32) {
+        match self.states.last_mut() {
+            Some(state) => state.controller_button_up_event(_btn, _instance_id),
+            None => (),
+        }
+    }
+    fn controller_axis_event(&mut self, _axis: Axis, _value: i16, _instance_id: i32) {
+        match self.states.last_mut() {
+            Some(state) => state.controller_axis_event(_axis, _value, _instance_id),
+            None => (),
+        }
+    }
+
+    fn focus_event(&mut self, _gained: bool) {
+        match self.states.last_mut() {
+            Some(state) => state.focus_event(_gained),
+            None => (),
+        }
+    }
+
+    /// Called upon a quit event.  If it returns true,
+    /// the game does not exit.
+    fn quit_event(&mut self) -> bool {
+        match self.states.last_mut() {
+            Some(state) => state.quit_event(),
+            None => false,
+        }
+    }
+}
