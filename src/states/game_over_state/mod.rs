470794dadb8f346e73a69f75a327b5d45b59d4c1
Dale Karp <dale@dale.io>
2017-06-21 18:35:47 -0400

    game over state now has a menu!

--- src/states/game_over_state/mod.rs
+++ src/states/game_over_state/mod.rs
@@ -2,20 +2,24 @@ use std::time::Duration;
 
 use ggez::{Context, GameResult, graphics};
 use ggez::event::{Mod, Keycode};
+use ggez::graphics::Point;
 
-use event::{Assets, Transition, EventHandler};
+use event::{Assets, Transition, EventHandler, Button};
 use states::play_state::PlayState;
 use states::menu_state::MenuState;
+use states::shared::option::{Option, OptionInputCommand};
 
 pub struct GameOverState {
-    request_menu: bool,
     request_replay: bool,
+    request_menu: bool,
+    request_quit: bool,
+    options: Vec<Option>,
+    current_selection: usize,
 
     game_over_text: graphics::Text,
     final_score_text: graphics::Text,
     final_line_text: graphics::Text,
     final_level_text: graphics::Text,
-    instruction_text: graphics::Text,
 }
 
 impl GameOverState {
@@ -27,10 +31,6 @@ impl GameOverState {
                -> GameResult<GameOverState> {
         let game_over = graphics::Text::new(ctx, "GAME OVER", assets.get_font("title")?)?;
 
-        let instruction_src = "'R' to restart / 'M' for menu / 'Esc' to quit";
-        let instruction_text =
-            graphics::Text::new(ctx, instruction_src, assets.get_font("normal")?)?;
-
         let score_str = format!("Final Score: {}", final_score_value);
         let lines_str = format!("Final Lines: {}", final_cleared);
         let level_str = format!("Final Level: {}", final_level);
@@ -38,16 +38,56 @@ impl GameOverState {
         let final_lines = graphics::Text::new(ctx, &lines_str, assets.get_font("normal")?)?;
         let final_level = graphics::Text::new(ctx, &level_str, assets.get_font("normal")?)?;
 
+        let coords = graphics::get_screen_coordinates(&ctx);
+        let mut options_vec: Vec<Option> = Vec::new();
+        options_vec.push(Option::new(ctx, assets, "Play again", Point::new(coords.w / 2.0, 450.0)));
+        options_vec.push(Option::new(ctx, assets, "Return to Menu", Point::new(coords.w / 2.0, 525.0)));
+        options_vec.push(Option::new(ctx, assets, "Quit", Point::new(coords.w / 2.0, 600.0)));
+
         Ok(GameOverState {
-            request_menu: false,
             request_replay: false,
+            request_menu: false,
+            request_quit: false,
+            options: options_vec,
+            current_selection: 0,
 
             game_over_text: game_over,
             final_score_text: final_score,
             final_line_text: final_lines,
             final_level_text: final_level,
-            instruction_text: instruction_text,
         })
+    }
+
+    fn handle_input(&mut self, command: OptionInputCommand) {
+        match command {
+            OptionInputCommand::Up => {
+                if self.current_selection <= 0 {
+                    self.current_selection = self.options.len() - 1;
+                }
+                else {
+                    self.current_selection -= 1;
+                }
+            },
+            OptionInputCommand::Down => {
+                if self.current_selection >= self.options.len() - 1{
+                    self.current_selection = 0;
+                }
+                else {
+                    self.current_selection += 1;
+                }
+            },
+            OptionInputCommand::Select => {
+                if self.current_selection == 0 {
+                    self.request_replay = true;
+                }
+                else if self.current_selection == 1 {
+                    self.request_menu = true;
+                }
+                else if self.current_selection == 2 {
+                    self.request_quit = true;
+                }
+            },
+        }
     }
 }
 
@@ -61,6 +101,16 @@ impl EventHandler for GameOverState {
             return Ok(Transition::Swap(Box::new(MenuState::new(ctx, assets)?)));
         } else if self.request_replay {
             return Ok(Transition::Swap(Box::new(PlayState::new(ctx, assets)?)));
+        } else if self.request_quit {
+            return Ok(Transition::Drain);
+        }
+
+        for (i, option) in self.options.iter_mut().enumerate() {
+            if i == self.current_selection {
+                option.update(true)?;
+            } else {
+                option.update(false)?;
+            }
         }
 
         Ok(Transition::None)
@@ -73,7 +123,6 @@ impl EventHandler for GameOverState {
         let game_over_score_dest = graphics::Point::new(coords.w / 2.0, 200.0);
         let game_over_lines_dest = graphics::Point::new(coords.w / 2.0, 250.0);
         let game_over_level_dest = graphics::Point::new(coords.w / 2.0, 300.0);
-        let game_over_end_dest = graphics::Point::new(coords.w / 2.0, 400.0);
 
         graphics::set_color(ctx, graphics::Color::new(0.0, 0.0, 0.0, 0.7))?;
         graphics::rectangle(ctx,
@@ -87,16 +136,32 @@ impl EventHandler for GameOverState {
         graphics::draw(ctx, &self.final_score_text, game_over_score_dest, 0.0)?;
         graphics::draw(ctx, &self.final_line_text, game_over_lines_dest, 0.0)?;
         graphics::draw(ctx, &self.final_level_text, game_over_level_dest, 0.0)?;
-        graphics::draw(ctx, &self.instruction_text, game_over_end_dest, 0.0)?;
 
+        for option in &self.options {
+            option.draw(ctx)?;
+        }
 
         Ok(())
     }
 
-    fn key_down_event(&mut self, keycode: Keycode, _keymod: Mod, _repeat: bool) {
+    fn key_down_event(&mut self, keycode: Keycode, _keymod: Mod, repeat: bool) {
+        if repeat {
+            return;
+        }
+
         match keycode {
-            Keycode::R => self.request_replay = true,
-            Keycode::M => self.request_menu = true,
+            Keycode::Up => self.handle_input(OptionInputCommand::Up),
+            Keycode::Down => self.handle_input(OptionInputCommand::Down),
+            Keycode::Return => self.handle_input(OptionInputCommand::Select),
+            _ => (),
+        }
+    }
+
+    fn controller_button_down_event(&mut self, btn: Button, _instance_id: i32) {
+        match btn {
+            Button::DPadUp => self.handle_input(OptionInputCommand::Up),
+            Button::DPadDown => self.handle_input(OptionInputCommand::Down),
+            Button::A => self.handle_input(OptionInputCommand::Select),
             _ => (),
         }
     }
