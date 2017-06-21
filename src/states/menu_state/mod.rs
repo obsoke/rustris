84fc40bbaaac3172122972f34e13c518b414fbfc
Dale Karp <dale@dale.io>
2017-06-20 20:06:46 -0400

    menu selection! mvp main menu!

--- src/states/menu_state/mod.rs
+++ src/states/menu_state/mod.rs
@@ -1,21 +1,43 @@
 use std::time::Duration;
 use ggez::{Context, GameResult, graphics};
+use ggez::graphics::Point;
 use event::{Assets, Transition, EventHandler, Keycode, Mod};
-use states::play_state::PlayState;
-//use states::play_state::tetromino::Piece;
+mod option;
+
+use self::option::Option;
+use states::play_state::{PlayState, Position};
+use states::play_state::tetromino::{Piece, PieceType};
 use util::DurationExt;
 
 pub struct MenuState {
     title_text: graphics::Text,
     title_rotation: f64,
+    piece: Piece,
+    options: Vec<Option>,
+    current_selection: usize,
+
+    request_play: bool,
+    request_exit: bool,
 }
 
 impl MenuState {
     pub fn new(ctx: &mut Context, assets: &Assets) -> GameResult<MenuState> {
         let title = graphics::Text::new(ctx, "Rustris", assets.get_font("title")?)?;
+        let mut options_vec: Vec<Option> = Vec::new();
+        let coords = graphics::get_screen_coordinates(&ctx);
+
+        options_vec.push(Option::new(ctx, assets, "Play!", Point::new(coords.w / 2.0, 250.0)));
+        options_vec.push(Option::new(ctx, assets, "Exit", Point::new(coords.w / 2.0, 325.0)));
+
         Ok(MenuState {
             title_text: title,
             title_rotation: 0.0,
+            piece: Piece::new(PieceType::L),
+            options: options_vec,
+            current_selection: 0,
+
+            request_play: false,
+            request_exit: false,
         })
     }
 }
@@ -26,26 +48,75 @@ impl EventHandler for MenuState {
               assets: &Assets,
               dt: Duration)
               -> GameResult<Transition> {
-        println!("MENU STATE STUB!");
         self.title_rotation += dt.as_subsec_millis();
+
+        if self.request_play {
+            return Ok(Transition::Swap(Box::new(PlayState::new(ctx, assets).unwrap())));
+        }
+        if self.request_exit {
+            return Ok(Transition::Pop);
+        }
+
+        // for mut option in &mut self.options {
+        for (i, option) in self.options.iter_mut().enumerate() {
+            if i == self.current_selection {
+                option.update(true)?;
+            } else {
+                option.update(false)?;
+            }
+        }
+
         Ok(Transition::None)
     }
 
-    fn draw(&mut self, ctx: &mut Context, _: &Assets) -> GameResult<()> {
+    fn draw(&mut self, ctx: &mut Context, assets: &Assets) -> GameResult<()> {
         let coords = graphics::get_screen_coordinates(&ctx);
 
         let title_dest = graphics::Point::new(coords.w / 2.0, 100.0);
+        let pos = Position::new(5, 50);
 
         graphics::set_color(ctx, graphics::Color::new(1.0, 1.0, 1.0, 1.0))?;
         graphics::draw(ctx, &self.title_text, title_dest, 0.0)?;
+        self.piece.draw_at_point(ctx, assets.get_image("block")?, pos, self.title_rotation)?;
+
+        for option in &self.options {
+            option.draw(ctx)?;
+        }
 
         Ok(())
     }
 
-    fn key_down_event(&mut self, keycode: Keycode, _keymod: Mod, _repeat: bool) {
+    fn key_down_event(&mut self, keycode: Keycode, _keymod: Mod, repeat: bool) {
+        if repeat {
+            return;
+        }
+
         match keycode {
-            // Keycode::R => self.request_replay = true,
-            // Keycode::M => self.request_menu = true,
+            Keycode::Up => {
+                if self.current_selection <= 0 {
+                    self.current_selection = self.options.len() - 1;
+                }
+                else {
+                    self.current_selection -= 1;
+                }
+            },
+            Keycode::Down => {
+                if self.current_selection >= self.options.len() - 1{
+                    self.current_selection = 0;
+                }
+                else {
+                    self.current_selection += 1;
+                }
+            },
+            Keycode::Return => {
+                println!("HI!");
+                if self.current_selection == 0 {
+                    self.request_play = true;
+                }
+                else if self.current_selection == 1 {
+                    self.request_exit = true;
+                }
+            },
             _ => (),
         }
     }
---
+++ src/states/menu_state/option.rs
@@ -0,0 +1,48 @@
+use ggez::{Context, GameResult, graphics};
+use ggez::graphics::{Point, Color};
+use event::Assets;
+use states::play_state::Position;
+/// A menu option.
+pub struct Option {
+    name: String,
+    text: graphics::Text,
+    centre: Point,
+    is_selected: bool,
+}
+
+impl Option {
+    pub fn new(ctx: &mut Context, assets: &Assets, name: &'static str, top_left: Point) -> Self {
+        let text = graphics::Text::new(ctx, name, assets.get_font("normal").unwrap()).unwrap();
+        Self {
+            name: String::from(name),
+            text: text,
+            centre: top_left,
+            is_selected: false,
+        }
+    }
+
+    pub fn get_name(&self) -> &String {
+        &self.name
+    }
+
+    pub fn update(&mut self, is_selected: bool) -> GameResult<()> {
+        if self.is_selected != is_selected {
+            self.is_selected = is_selected;
+        }
+        Ok(())
+    }
+
+    pub fn draw(&self, ctx: &mut Context) -> GameResult<()> {
+        if self.is_selected {
+            graphics::set_color(ctx, Color::new(1.0, 1.0, 0.0, 1.0))?;
+        } else {
+            graphics::set_color(ctx, Color::new(1.0, 1.0, 1.0, 1.0))?;
+        }
+
+        graphics::draw(ctx, &self.text, self.centre, 0.0)?;
+
+        Ok(())
+    }
+}
+
+
